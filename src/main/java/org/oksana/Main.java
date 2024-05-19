bd2f076b89d0ed3c342c73b8c9cb5cfeb6eb31c6
Oksana Chumakova <okchumakova@gmail.com>
2024-05-19 14:59:13 +1000

    minor changes

--- src/main/java/org/oksana/Main.java
+++ src/main/java/org/oksana/Main.java
@@ -3,15 +3,33 @@ package org.oksana;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static boolean isValid(String inputString) {
+
+        boolean wasBMet = false;
+        boolean isStringValid = true;
+        for (String ch : inputString.split("")) {
+
+            if (ch.equals("b")) {
+                wasBMet = true;
+            }
+
+            if (wasBMet && ch.equals("a")) {
+                isStringValid = false;
+            }
         }
+        return isStringValid;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("abba - " + isValid("abba"));
+        System.out.println("abb - " + isValid("abb"));
+        System.out.println("aaa - " + isValid("aaa"));
+        System.out.println("bbb - " + isValid("bbb"));
+        System.out.println("a - " + isValid("a"));
+        System.out.println("b - " + isValid("b"));
+        System.out.println("aaaaaaaaaaaab - " + isValid("aaaaaaaaaaaab"));
+        System.out.println("bbbbbbbbbbbbba - " + isValid("bbbbbbbbbbbbba"));
+
     }
 }
